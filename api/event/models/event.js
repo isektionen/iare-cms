16257f453dbb9c3ae100d59ea2b7789077d017ef
landeholt <johnlan@kth.se>
2021-06-15 20:20:23 +0200

    Added event-password-component, datetime-validation, owner-validation & event-ticket-component. closes #1, #2, #3, #4

--- api/event/models/event.js
+++ api/event/models/event.js
@@ -1,8 +1,44 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#lifecycle-hooks)
  * to customize this model
  */
 
-module.exports = {};
+const getCommitteesByID = async (id) => {
+  const adminUsers = await strapi.services.committee.deepRelation({
+    "adminUsers.id": id,
+  });
+
+  const users = await strapi.services.committee.deepRelation({
+    "users.id": id,
+  });
+  return [...adminUsers, ...users];
+};
+
+module.exports = {
+  lifecycles: {
+    async beforeCreate(data) {
+      const errors = await strapi.services.event.validate(data);
+      if (errors.length > 0) {
+        // Strapi doesn't allow you to push multiple errors
+        throw strapi.errors.badRequest(errors[0].message);
+      }
+    },
+    async afterCreate(result, data) {
+      if (!result.committee) {
+        const { id } = result.created_by;
+        const committees = await getCommitteesByID(id);
+        if (committees.length > 0) {
+          // TODO: find a nicer solution to auto-appending a committee when none was added
+          await strapi.query("event").update(
+            { id: result.id },
+            {
+              committee: committees[0].id,
+            }
+          );
+        }
+      }
+    },
+  },
+};
