de8b49f702f49d35efb1191873e85047b46086d4
landeholt <johnlan@kth.se>
2021-06-16 10:32:22 +0200

    added endpoint for event password validation. closes #6

--- api/event/controllers/event.js
+++ api/event/controllers/event.js
@@ -5,4 +5,23 @@
  * to customize this controller
  */
 
-module.exports = {};
+const { parseMultipartData } = require("strapi-utils");
+
+module.exports = {
+  async validatePassword(ctx) {
+    const { id } = ctx.params;
+    const event = await strapi.query("event").findOne({ id });
+    const body = ctx.request?.body ?? {};
+    const maybePassword = body?.password ?? null;
+    if (maybePassword && event) {
+      if (event.passwordProtected) {
+        const { password } = event.passwordProtected;
+        return {
+          id,
+          valid: password === maybePassword,
+        };
+      }
+    }
+    ctx.response.status = 400;
+  },
+};
