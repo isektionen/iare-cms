3994ef928c5b88ee203bf67aafdd7e40b68015a5
landeholt <johnlan@kth.se>
2021-07-11 13:11:18 +0200

    working on issues of deployment

--- config/server.js
+++ config/server.js
@@ -1,12 +1,12 @@
 module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
-  url: "https://iare-cms.herokuapp.com/",
+  url: env("BACKEND_URL", "/"),
   admin: {
     auth: {
       secret: env("ADMIN_JWT_SECRET", "1438656bfa7261c4ba88e07fde3039b7"),
     },
-    url: env("PUBLIC_ADMIN_URL", "/"),
+    url: env("PUBLIC_ADMIN_URL", "/admin"),
     serveAdminPanel: env.bool("SERVE_ADMIN", true),
   },
 });
