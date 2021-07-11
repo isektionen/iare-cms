2b1c515388a6a32163cc35522e500922d24d85a7
landeholt <johnlan@kth.se>
2021-07-11 11:25:27 +0200

    built admin

--- config/server.js
+++ config/server.js
@@ -1,9 +1,12 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
+  url: "https://iare-cms.herokuapp.com/",
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', '1438656bfa7261c4ba88e07fde3039b7'),
+      secret: env("ADMIN_JWT_SECRET", "1438656bfa7261c4ba88e07fde3039b7"),
     },
+    url: "/",
+    serverAdminPanel: false,
   },
 });
