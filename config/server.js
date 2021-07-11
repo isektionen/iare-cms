510e45c2f23c0a7a12c1ad607231217529bc832b
landeholt <johnlan@kth.se>
2021-07-11 13:21:05 +0200

    working on issues of deployment

--- config/server.js
+++ config/server.js
@@ -1,12 +1,12 @@
 module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
-  url: env("BACKEND_URL", "/"),
+  //url: env("BACKEND_URL", "/"),
   admin: {
     auth: {
       secret: env("ADMIN_JWT_SECRET", "1438656bfa7261c4ba88e07fde3039b7"),
     },
-    url: env("PUBLIC_ADMIN_URL", "/admin"),
-    serveAdminPanel: env.bool("SERVE_ADMIN", true),
+    //url: env("PUBLIC_ADMIN_URL", "/admin"),
+    //serveAdminPanel: env.bool("SERVE_ADMIN", true),
   },
 });
