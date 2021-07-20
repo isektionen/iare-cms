3ecd8e1041cacdb90364da2075b39904ea2c920b
landeholt <johnlan@kth.se>
2021-07-20 22:18:04 +0200

    added website to company collection

--- api/jobs/controllers/jobs.js
+++ api/jobs/controllers/jobs.js
@@ -1,8 +1,21 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
+const { parseMultipartData, sanitizeEntity } = require("strapi-utils");
+const sanitizeId = (value) => {
+  if (/^-?\d+$/.test(value)) return { id: value };
+  return { slug: value };
+};
 
-module.exports = {};
+module.exports = {
+  async findOne(ctx) {
+    const { id } = ctx.params;
+
+    const entity = await strapi.services.jobs.findOne(sanitizeId(id));
+
+    return sanitizeEntity(entity, { model: strapi.models.jobs });
+  },
+};
