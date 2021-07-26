680d6f38067bd94dbfe5c3db0b6d0579ee770bfa
landeholt <johnlan@kth.se>
2021-07-26 03:32:31 +0200

    prod issues

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,6 +1,11 @@
 module.exports = ({ env }) => ({
+  host: "0.0.0.0",
   url: env("BACKEND_URL"),
+  port: 443,
   admin: {
     autoOpen: env.bool("ADMIN_AUTO_OPEN", false),
+    auth: {
+      secret: env("ADMIN_JWT_SECRET", "1438656bfa7261c4ba88e07fde3039b7"),
+    },
   },
 });
