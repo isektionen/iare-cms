db2709f8de046e7ce3753c1203e287df40cc57d2
landeholt <johnlan@kth.se>
2021-07-26 15:08:47 +0200

    prod issues

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -3,6 +3,7 @@ module.exports = ({ env }) => ({
   url: env("BACKEND_URL"),
   port: env.int("PORT", 1337),
   admin: {
+    url: "/dashboard",
     autoOpen: env.bool("ADMIN_AUTO_OPEN", false),
     auth: {
       secret: env("ADMIN_JWT_SECRET", "1438656bfa7261c4ba88e07fde3039b7"),
