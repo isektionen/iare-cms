3fc8c94cb1db6e62c836de9079cedab5bf31d0c3
landeholt <johnlan@kth.se>
2021-07-28 12:49:48 +0200

    making playground available in prod

--- config/env/production/plugins.js
+++ config/env/production/plugins.js
@@ -10,4 +10,14 @@ module.exports = ({ env }) => ({
       },
     },
   },
+  graphql: {
+    endpoint: "/graphql",
+    shadowCRUD: true,
+    playgroundAlways: true,
+    depthLimit: 7,
+    amountLimit: 100,
+    apolloServer: {
+      tracing: false,
+    },
+  },
 });
