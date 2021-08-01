934750549783091f7023eb10136e588123634860
landeholt <johnlan@kth.se>
2021-08-01 18:48:33 +0200

    trying to find a logic state where a sequence of events occur without breaking the state machine

--- api/event/models/event.js
+++ api/event/models/event.js
@@ -39,7 +39,7 @@ const createUIDs = (result) => {
 
 var state = [];
 var persistedIDs = [];
-
+/*
 const syncLocales = async ({ data, result, id }) => {
   if (
     persistedIDs.includes(id) ||
@@ -60,7 +60,8 @@ const syncLocales = async ({ data, result, id }) => {
       ...state,
       {
         id: result.id,
-        tickets: data.tickets.Tickets,
+        tickets: data.tickets,
+        ticketContainerId: data.id,
         locales: result.localizations.map((l) => l.id),
       },
     ];
@@ -70,17 +71,25 @@ const syncLocales = async ({ data, result, id }) => {
     persistedIDs.push(id);
     const _state = state.find((obj) => obj.locales.includes(id));
     if (!_state) return;
-    const { id: _id, tickets, locales = [] } = _state;
+    const {
+      id: _id,
+      tickets,
+      ticketContainerId,
+      locales = [],
+    } = _.cloneDeep(_state);
 
-    state = state.filter((obj) => obj !== _state);
+    _.remove(state, (obj) => (obj.id = _state.id));
+
     const syncUIDs = (tickets, key) => {
-      if (key === "null" || key === "undefined") {
-        const uuid = uuidv4();
-        const ticket = { ..._.first(tickets), ticketUID: uuid };
+      if (key === "null" || key === "undefined" || tickets.length === 1) {
+        const _ticket = _.first(tickets);
+        const uuid = _ticket.ticketUID ? _ticket.ticketUID : uuidv4();
+        const ticket = { ..._ticket, ticketUID: uuid };
         const amount = tickets.length;
         const others = _.fill(Array(amount), {
-          ..._.omit(ticket, "id", "belongsTo"),
+          ..._.omit(ticket, "id"),
           belongsTo: id,
+          container: result.tickets.id,
         });
         return [ticket, ...others];
       }
@@ -89,10 +98,17 @@ const syncLocales = async ({ data, result, id }) => {
 
     if (locales.includes(id)) {
       // match existing tickets
-      console.log(tickets);
-      const payload = _.chain([
-        ...tickets.map((t) => ({ ...t, belongsTo: _id })),
-        ...result.tickets.Tickets.map((t) => ({ ...t, belongsTo: id })),
+      let payload = _.chain([
+        ...tickets.map((t) => ({
+          ...t,
+          belongsTo: _id,
+          container: ticketContainerId,
+        })),
+        ...result.tickets.Tickets.map((t) => ({
+          ...t,
+          belongsTo: id,
+          container: result.tickets.id,
+        })),
       ])
         .groupBy("ticketUID")
         .mapValues(syncUIDs)
@@ -103,23 +119,128 @@ const syncLocales = async ({ data, result, id }) => {
         .toPairs()
         .value();
 
+      if (_.isEmpty(payload)) {
+        payload = [_id, ...locales].map((i) => [i, []]);
+      }
       const update = strapi.query("event").update;
-
-      payload.forEach(
-        async ([id, tickets]) =>
-          await update(
-            { id },
-            {
-              tickets: {
-                Tickets: tickets.map((ticket) => _.omit(ticket, "belongsTo")),
-              },
-            }
-          )
+      const res = await Promise.all(
+        payload.map(
+          async ([id, tickets]) =>
+            await update(
+              { id },
+              {
+                tickets: {
+                  Tickets: tickets.map((ticket) =>
+                    _.omit(ticket, "belongsTo", "container")
+                  ),
+                },
+              }
+            )
+        )
       );
     }
   }
 };
+*/
 
+const insertUID = (r) => ({
+  ...r,
+  ticketUID: r.ticketUID ? r.ticketUID : uuidv4(),
+});
+
+const getLocales = (records) => records.localizations.map((r) => r.id);
+
+const syncLocales = async ({ data, result, id }) => {
+  const update = strapi.query("event").update;
+  // when state is empty, populate ticketUID for all recieved tickets
+  if (result && data && !state.some((obj) => obj.eventId === result.id)) {
+    console.log("add to state", result.locale);
+    result.tickets.Tickets = _.map(result.tickets.Tickets, insertUID);
+    state.push({
+      eventId: result.id,
+      tickets: result.tickets,
+      locales: getLocales(result),
+    });
+    const _result = await update(
+      { id: result.id },
+      { tickets: result.tickets }
+    );
+    if (_result.id === result.id) {
+      result = {
+        ..._result,
+        slug: result.slug,
+        locale: result.locale,
+        localizations: result.localizations,
+        orders: result.orders,
+      };
+    }
+  }
+  if (id) {
+    console.log("find state", result.locale);
+    const _state = state.find((obj) => obj.locales.includes(id));
+    // sync tickets from previous state
+    _.remove(state, (obj) => obj.eventId === _state.eventId);
+    if (_.isEmpty(_state)) return;
+
+    const sync = (tickets, uid) => {
+      if (["null", "undefined"].includes(uid) || tickets.length === 1) {
+        // ticket(s) doesn't have a uid attached to it
+
+        const _ticket = _.first(tickets);
+
+        const uuid = _ticket.ticketUID ? _ticket.ticketUID : uuidv4();
+        const ticket = { ..._ticket, ticketUID: uuid };
+        const others = _.fill(Array(locales.length), {
+          ...ticket,
+          belongsTo: id,
+        });
+        return [ticket, ...others];
+      }
+      return tickets;
+    };
+
+    const { tickets = [], locales = [] } = _state;
+    if (locales.includes(id)) {
+      console.log("sync state", result.locale);
+      const otherLocaleTickets = tickets.Tickets.map((ticket) =>
+        _.omit(ticket, "id")
+      );
+      const localeTickets = result.tickets.Tickets.map((ticket) => ({
+        ...ticket,
+        belongsTo: id,
+      }));
+
+      let _data = _.chain([...otherLocaleTickets, ...localeTickets])
+        .groupBy("ticketUID")
+        .omitBy((v) => v.length === 1 && _.has(_.first(v), "belongsTo"))
+        .mapValues(sync)
+        .values()
+        .flatten()
+        .filter((v) => v.belongsTo === id)
+        .groupBy("belongsTo")
+        .toPairs()
+        .value();
+
+      const [_result] = await Promise.all(
+        _data.map(
+          async ([id, tickets]) =>
+            await update(
+              { id: Number.parseInt(id) },
+              {
+                tickets: {
+                  id: result.tickets.id,
+                  Tickets: tickets.map((ticket) => _.omit(ticket, "belongsTo")),
+                },
+              }
+            )
+        )
+      );
+      if (_result.id === result.id) {
+        //result = _result;
+      }
+    }
+  }
+};
 module.exports = {
   lifecycles: {
     async beforeCreate(data) {
@@ -144,12 +265,28 @@ module.exports = {
       }
     },
     async afterUpdate(result, params, data) {
+      if (
+        _.chain(data).keys().isEqual(["fullfillmentUID", "tickets"]).value()
+      ) {
+        return;
+      }
       if (_.has(data, "tickets.Tickets")) {
+        /*const tickets = _.cloneDeep(
+          data.tickets.Tickets.map((ticket) => ({
+            ...ticket,
+            ticketUID: ticket.ticketUID ? ticket.ticketUID : uuidv4(),
+          }))
+        );
+
+        await syncLocales({
+          data: { id: data.tickets.id, tickets },
+          result,
+        });
+        result.tickets.Tickets = result.tickets.Tickets.map((t, i) => ({
+          ...t,
+          ticketUID: tickets[i].ticketUID,
+        }));*/
         await syncLocales({ data, result });
-        result.tickets.Tickets = result.tickets.Tickets.map((ticket) => ({
-          ...ticket,
-          ticketUID: ticket.ticketUID ? ticket.ticketUID : uuidv4(),
-        }));
       } else {
         await syncLocales({ result, id: params.id });
       }
