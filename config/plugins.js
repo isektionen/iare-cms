021c8d0d504b4b31fd1eb3f17f2a5b4f9755b8f9
landeholt <johnlan@kth.se>
2021-08-04 21:36:37 +0200

    added gsuite

--- config/plugins.js
+++ config/plugins.js
@@ -2,13 +2,13 @@ module.exports = ({ env }) => ({
   email: {
     provider: "gmail-2lo",
     providerOptions: {
-      username: "no-reply@indek.se",
+      username: "no-reply@iare.se",
       clientId: env("EMAIL_CLIENT_ID"),
       privateKey: env("EMAIL_PRIVATE_KEY").replace(/\\n/g, "\n"),
     },
     settings: {
-      defaultFrom: "no-reply@indek.se",
-      defaultReplyTo: "no-reply@indek.se",
+      defaultFrom: "no-reply@iare.se",
+      defaultReplyTo: "no-reply@iare.se",
     },
   },
 });
