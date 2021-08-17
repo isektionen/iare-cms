c85b64cb3327c7b060ad73231507b642170cdbb4
landeholt <johnlan@kth.se>
2021-08-17 12:46:39 +0200

    added logo to footer type

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -3,7 +3,7 @@ module.exports = ({ env }) => ({
   url: env("BACKEND_URL"),
   port: env.int("PORT", 1337),
   admin: {
-    url: "/admin",
+    url: "https://cms.iare.se/",
     autoOpen: env.bool("ADMIN_AUTO_OPEN", false),
     auth: {
       secret: env("ADMIN_JWT_SECRET", "1438656bfa7261c4ba88e07fde3039b7"),
