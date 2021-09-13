503d04e91332ff292d5abab202123751fa54e49f
landeholt <johnlan@kth.se>
2021-09-13 13:46:24 +0200

    corrected search with lowercase only

--- plugins/event-manager/admin/src/containers/Event/index.js
+++ plugins/event-manager/admin/src/containers/Event/index.js
@@ -162,13 +162,6 @@ const Event = () => {
     updateData((b) => !b);
   };
 
-  const toLowerCase = useCallback((str) => {
-    try {
-      return str.toLowerCase();
-    } catch (e) {
-      return str;
-    }
-  }, []);
   const filteredOrders = useMemo(() => {
     if (orders && orders.length > 0) {
       return orders.filter((order) => {
@@ -185,6 +178,8 @@ const Event = () => {
     }
     return orders;
   }, [orders]);
+
+  console.log(filteredOrders);
 
   useEffect(() => {
     if (loaded && !eventExists) {
