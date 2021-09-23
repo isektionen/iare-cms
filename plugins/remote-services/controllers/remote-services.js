3268d50205d1ba7cf2975b57244bc03fa3e93d95
landeholt <johnlan@kth.se>
2021-09-23 13:29:30 +0200

    added purge service

--- plugins/remote-services/controllers/remote-services.js
+++ plugins/remote-services/controllers/remote-services.js
@@ -106,13 +106,13 @@ module.exports = {
     );
   },
   purge: async (ctx) => {
-    // find all orders that are not success and 1 day old.
+    // find all orders that are not success and 2 day old.
     const today = new Date();
-    const yesterday = subDays(today, 1);
+    const yesterday = subDays(today, 2);
     const dateFormatted = formatISO(yesterday);
-    const idArray = await strapi
+    const orders = await strapi
       .query("order")
-      .find({ created_at_lt: dateFormatted, status_nin: ["success"] });
-    console.log(idArray);
+      .delete({ created_at_lt: dateFormatted, status_nin: ["success"] });
+    console.log(orders);
   },
 };
