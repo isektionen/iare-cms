2a8bd9a514f034cd97b70c77223170ebbcd9d877
landeholt <johnlan@kth.se>
2021-09-23 15:02:02 +0200

    added added count & maxTickets

--- api/order/config/schema.graphql.js
+++ api/order/config/schema.graphql.js
@@ -13,7 +13,7 @@ module.exports = {
       order: false,
       ordersCount: {
         description: "Return the count of orders",
-        resolverOf: "application::order.order.find",
+        resolverOf: "application::order.order.details",
         resolver: async (obj, options, ctx) => {
           const where = { event: options.where.event, status: "success" };
           return await strapi.query("order").count(where);
