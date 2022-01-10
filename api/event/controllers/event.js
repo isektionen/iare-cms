d7568f76c5cef29e16a6b7b2e4df749b756f1caf
landeholt <johnlan@kth.se>
2022-01-10 18:15:34 +0100

    feat: 🚧 added endpoints for products, status and reservation

--- api/event/controllers/event.js
+++ api/event/controllers/event.js
@@ -5,72 +5,115 @@
  * to customize this controller
  */
 const { parseMultipartData, sanitizeEntity } = require("strapi-utils");
-
+const _ = require("lodash");
+const { isAfter, parseISO } = require("date-fns");
 const sanitizeId = (value) => {
-  if (/^-?\d+$/.test(value)) return { id: value };
+	if (/^-?\d+$/.test(value)) return { id: value };
 
-  const nano = new RegExp(/^[A-Za-z0-9_-]{8}$/i);
-  if (nano.test(value)) return { fullfillmentUID: value };
-  return { slug: value };
+	const nano = new RegExp(/^[A-Za-z0-9_-]{8}$/i);
+	if (nano.test(value)) return { fullfillmentUID: value };
+	return { slug: value };
 };
 
 const isAdmin = async (ctx) => {
-  if (ctx && ctx.request && ctx.request.header) {
-    if (ctx.request.header.authorization) {
-      const authorization = ctx.request.header.authorization;
-      const parts = authorization.split(" ");
-      if (parts.length === 2) {
-        const scheme = parts[0];
-        const credentials = parts[1];
-        if (/^Bearer$/i.test(scheme)) {
-          const { isValid } = await strapi.admin.services.token.decodeJwtToken(
-            credentials
-          );
-          return isValid;
-        }
-      }
-    }
-  }
-  return false;
+	if (ctx && ctx.request && ctx.request.header) {
+		if (ctx.request.header.authorization) {
+			const authorization = ctx.request.header.authorization;
+			const parts = authorization.split(" ");
+			if (parts.length === 2) {
+				const scheme = parts[0];
+				const credentials = parts[1];
+				if (/^Bearer$/i.test(scheme)) {
+					const { isValid } =
+						await strapi.admin.services.token.decodeJwtToken(
+							credentials
+						);
+					return isValid;
+				}
+			}
+		}
+	}
+	return false;
 };
 
 module.exports = {
-  async validatePassword(ctx) {
-    const { id } = ctx.params;
-    const event = await strapi.query("event").findOne({ id });
-    const body = ctx.request?.body ?? {};
-    const maybePassword = body?.password ?? null;
-    if (maybePassword && event) {
-      if (event.passwordProtected) {
-        const { password } = event.passwordProtected;
-        return {
-          valid: password === maybePassword,
-        };
-      }
-    }
-    ctx.response.status = 400;
-  },
-  async findOneBySlug(ctx) {
-    const { _slug } = ctx.params;
-    const event = await strapi.query("event").findOne({ slug: _slug });
-    return event;
-  },
-  async find(ctx) {
-    let entities;
-    if (ctx.query._q) {
-      entities = await strapi.services.event.search(ctx.query);
-    } else {
-      entities = await strapi.services.event.find(ctx.query);
-    }
-    return entities.map((entity) =>
-      sanitizeEntity(entity, { model: strapi.models.event })
-    );
-  },
-  async findOne(ctx) {
-    const { id } = ctx.params;
-    //const admin = await isAdmin(ctx);
-    const entity = await strapi.services.event.findOne(sanitizeId(id));
-    //if (!admin) delete entity.tickets;
-    return sanitizeEntity(entity, { model: strapi.models.event });
-  },
+	async validatePassword(ctx) {
+		const { id } = ctx.params;
+		const event = await strapi.query("event").findOne({ id });
+		const body = ctx.request?.body ?? {};
+		const maybePassword = body?.password ?? null;
+		if (maybePassword && event) {
+			if (event.passwordProtected) {
+				const { password } = event.passwordProtected;
+				return {
+					valid: password === maybePassword,
+				};
+			}
+		}
+		ctx.response.status = 400;
+	},
+	async findOneBySlug(ctx) {
+		const { _slug } = ctx.params;
+		const event = await strapi.query("event").findOne({ slug: _slug });
+		return event;
+	},
+	async find(ctx) {
+		let entities;
+		if (ctx.query._q) {
+			entities = await strapi.services.event.search(ctx.query);
+		} else {
+			entities = await strapi.services.event.find(ctx.query);
+		}
+		return entities.map((entity) =>
+			sanitizeEntity(entity, { model: strapi.models.event })
+		);
+	},
+	async findOne(ctx) {
+		const { id } = ctx.params;
+		//const admin = await isAdmin(ctx);
+		const entity = await strapi.services.event.findOne(sanitizeId(id));
+		//if (!admin) delete entity.tickets;
+		return sanitizeEntity(entity, { model: strapi.models.event });
+	},
+
+	// returns a events available products
+	async products(ctx) {
+		const { ref } = ctx.params;
+		const entity = await strapi.services.event.findOne({ slug: ref });
+		if (!entity) {
+			return ctx.response.badRequest();
+		}
+		return entity.products.map((obj) =>
+			_.omit(obj, [
+				"media",
+				"description",
+				"created_by",
+				"updated_by",
+				"created_at",
+				"updated_at",
+				"published_at",
+			])
+		);
+	},
+
+	// returns whether event is bookable
+	async status(ctx) {
+		const { ref } = ctx.params;
+		const entity = await strapi.services.event.findOne({ slug: ref });
+		if (!entity) {
+			return ctx.response.badRequest();
+		}
+
+		const { schedule } = entity;
+		if (!schedule || !schedule.deadline) {
+			return ctx.response.badRequest();
+		}
+		const deadline = parseISO(schedule.deadline);
+		const now = new Date();
+		if (isAfter(now, deadline)) {
+			return ctx.response.badRequest();
+		}
+
+		ctx.response.status = 200;
+	},
 };
--- api/product/controllers/product.js
+++ api/product/controllers/product.js
@@ -1,8 +1,35 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+async function getEntity(ctx) {
+	const entity = await strapi.services.product.findOne({
+		reference: ctx.params.ref,
+	});
+	if (!entity) {
+		return ctx.response.badRequest();
+	}
+	return entity;
+}
+
+module.exports = {
+	async reserve(ctx) {
+		const entity = await getEntity(ctx);
+		const qty = parseInt(ctx?.query?.quantity ?? 0);
+		const itemsLeft = parseInt(entity.stock - entity.count);
+		if (qty <= itemsLeft && entity.count <= entity.stock) {
+			await strapi
+				.query("product")
+				.update(
+					{ reference: ctx.params.ref },
+					{ count: parseInt(entity.count) + qty }
+				);
+			ctx.response.status = 202;
+			return;
+		}
+		ctx.response.status = 403;
+	},
+};
